346a647afd5c4773a5af0663fbfff0abd23e7e33
darkeclipz <larsrotgers@gmail.com>
2018-12-30 23:07:48 +0100

    compose2, compose3

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import functools
 
 # Create a function composition of multiple functions.
+def compose2(f, g): return lambda x: f(g(x))
+def compose3(f, g, h): return lambda x: f(g(h(x)))
 def compose(*fs):
     return functools.reduce(lambda f, g: lambda x: f(g(x)), fs, lambda x: x)
 
